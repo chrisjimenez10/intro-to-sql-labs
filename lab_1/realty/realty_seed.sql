ed984346fd232b936cac2422819eff0f89177968
chris <jimenezchristopher5@gmail.com>
2024-06-04 16:27:25 -0500

    inserted all seed data

--- lab_1/realty/realty_seed.sql
+++ lab_1/realty/realty_seed.sql
@@ -11,3 +11,18 @@ INSERT INTO apartments (tenant, price, landlord_name) VALUES (TRUE, '$750000', '
 INSERT INTO apartments (tenant, price, landlord_name) VALUES (TRUE, '$350000', 'Tina Camila');
 INSERT INTO apartments (tenant, price, landlord_name) VALUES (FALSE, '$950000', 'Pablo Pueblo');
 INSERT INTO apartments (tenant, price, landlord_name) VALUES (TRUE, '$850000', 'Troy Newton');
+
+INSERT INTO offices (bathrooms, cubicles, company) VALUES (2, 500, 'Google');
+INSERT INTO offices (bathrooms, cubicles, company) VALUES (1, 450, 'Apple');
+INSERT INTO offices (bathrooms, cubicles, company) VALUES (4, 600, 'Tesla');
+INSERT INTO offices (bathrooms, cubicles, company) VALUES (5, 850, 'Salesforce');
+INSERT INTO offices (bathrooms, cubicles, company) VALUES (1, 350, 'Amazon');
+INSERT INTO offices (bathrooms, cubicles, company) VALUES (1, 250, 'Netflix');
+
+INSERT INTO storefronts (kitchens, square_footage, outdoor_seating, company_id) VALUES (TRUE, 800, TRUE, 1);
+INSERT INTO storefronts (kitchens, square_footage, outdoor_seating, company_id) VALUES (TRUE, 950, False, 2);
+INSERT INTO storefronts (kitchens, square_footage, outdoor_seating, company_id) VALUES (TRUE, 750, TRUE, 3);
+INSERT INTO storefronts (kitchens, square_footage, outdoor_seating, company_id) VALUES (TRUE, 550, FALSE, 4);
+INSERT INTO storefronts (kitchens, square_footage, outdoor_seating, company_id) VALUES (TRUE, 600, TRUE, 5);
+INSERT INTO storefronts (kitchens, square_footage, outdoor_seating, company_id) VALUES (TRUE, 900, TRUE, 6);
+INSERT INTO storefronts (kitchens, square_footage, outdoor_seating, company_id) VALUES (TRUE, 700, TRUE, 2);
