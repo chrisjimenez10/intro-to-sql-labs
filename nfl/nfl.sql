656f316a0b3995ab366c5c5a9bb37117f982b784
chris <jimenezchristopher5@gmail.com>
2024-06-03 20:07:00 -0500

    completed nfl lab

--- nfl/nfl.sql
+++ nfl/nfl.sql
@@ -180,17 +180,201 @@
 (21 rows)
 
 -- 9. The player with the highest salary in the NFL
- 
+-- Query: nfl=# SELECT * FROM players ORDER BY salary DESC limit 1;
+-- Output:  id  |      name      | position |  salary  | team_id
+-----+----------------+----------+----------+---------
+ 589 | Peyton Manning | QB       | 18000000 |      13
+(1 row)
+
 -- 10. The name and position of the first 100 players with the lowest salaries
-  
+-- Query: nfl=# SELECT name, position, salary FROM players ORDER BY salary ASC LIMIT 100;
+-- Output:           name          | position | salary
+------------------------+----------+--------
+ Phillip Dillard        |          | 369480
+ Eric Kettani           | RB       | 390000
+ Austin Sylvester       | RB       | 390000
+ Greg Orton             | WR       | 390000
+ Jerrod Johnson         | QB       | 390000
+ McLeod Bethel-Thompson | QB       | 390000
+ Jonathan Crompton      | QB       | 390000
+ Travon Bellamy         | CB       | 390000
+ Caleb King             | RB       | 390000
+ Mike Mohamed           | LB       | 390000
+ Kyle Nelson            | LS       | 390000
+ John Malecki           | G        | 390000
+ Nathan Bussey          | LB       | 390000
+ Robert James           | LB       | 390000
+ Markell Carter         | DE       | 390000
+ Aaron Lavarias         | DT       | 390000
+ Mark Dell              | WR       | 390000
+ Ronald Johnson         | WR       | 390000
+ Doug Worthington       | DT       | 390000
+ Derrick Jones          | WR       | 390000
+ Sealver Siliga         | DT       | 390000
+ Chase Beeler           | C        | 390000
+ Kenny Wiggins          | T        | 390000
+ Konrad Reuland         | TE       | 390000
+ Michael Wilhoite       | LB       | 390000
+ Garrett Chisolm        | G        | 390000
+ Juamorris Stewart      | WR       | 390000
+ Chad Spann             | RB       | 390000
+ Trevis Turner          | T        | 390000
+ Justin Medlock         | KR       | 390000
+ Armon Binns            | WR       | 390000
+ Derek Hall             | T        | 390000
+ Shaky Smithson         | WR       | 390000
+ Armando Allen          | RB       | 390000
+ DAndre Goodwin         | WR       | 390000
+ Jeremy Beal            | DE       | 390000
+ Brett Brackett         | TE       | 390000
+ Shaun Draughn          | RB       | 390000
+ John Clay              | RB       | 390000
+ Tristan Davis          | RB       | 390000
+ Curtis Holcomb         | CB       | 390000
+ Jimmy Young            | WR       | 390000
+ Kevin Cone             | WR       | 390000
+ Cory Nelms             | CB       | 390000
+ Ben Guidugli           | TE       | 390000
+ David Gilreath         | WR       | 390000
+ Dontavia Bogan         | WR       | 390000
+ Joe Hastings           | WR       | 390000
+ Marshall McFadden      | LB       | 390000
+ Kade Weston            | DT       | 390000
+ Kyle Hix               | T        | 390000
+ Mark LeGree            | S        | 390000
+ Mike Hartline          | QB       | 390000
+ Jameson Konz           | WR       | 390000
+ Tyler Beiler           |          | 390000
+ Mike Blanc             | DT       | 390000
+ Corbin Bryant          | DT       | 390000
+ Michael Jasper         | DT       | 390000
+ Mike Rivera            | LB       | 390000
+ Pat Devlin             | QB       | 390000
+ Jerome Messam          | RB       | 390000
+ Jamie McCoy            | TE       | 390000
+ Lestar Jean            | WR       | 390000
+ Malcolm Williams       | CB       | 390000
+ Ricky Sapp             | DE       | 390000
+ Rashad Carmichael      | CB       | 390000
+ Alex Silvestro         | DT       | 390000
+ Adam Weber             | QB       | 390000
+ Adam Grant             | T        | 390000
+ Jammie Kirlew          | DE       | 390000
+ Joe Reitz              | T        | 405000
+ Brandon Browner        | CB       | 415000
+ Jeff Byers             | C        | 415000
+ Jed Collins            | TE       | 415000
+ Aaron Berry            | CB       | 415000
+ Logan Payne            | WR       | 415000
+ Marcus Sherels         | CB       | 415000
+ T.J. Conley            | PR       | 415000
+ Cameron Sheffield      | LB       | 415000
+ Kyle Bosworth          | LB       | 415000
+ Garrett McIntyre       | DT       | 415000
+ John Estes             | C        | 415000
+ Thomas Austin          | G        | 415000
+ Emmanuel Stephens      | DT       | 415000
+ Markus White           | DE       | 440000
+ Ricardo Matthews       | DT       | 450000
+ Quinten Lawrence       | WR       | 465000
+ Bilal Powell           | RB       | 465000
+ Taylor Boggs           | C        | 465000
+ Kamar Aiken            | WR       | 465000
+ Dexter Jackson         | WR       | 465000
+ Justin Rogers          | CB       | 465000
+ Johnny White           | RB       | 465000
+ Eron Riley             | WR       | 465000
+ Tracy Wilson           | CB       | 465000
+ DaNorris Searcy        | S        | 465000
+ Chris White            | LB       | 465000
+ Sterling Moore         | CB       | 465000
+ Chris Hairston         | T        | 465000
+ Andrew Hawkins         | WR       | 465000
+(100 rows)
 
 -- 11. The average salary for a DE in the nfl
-  
+-- Query: nfl=# SELECT AVG(salary) FROM players WHERE position = 'DE';
+-- Output:          avg
+----------------------
+ 2161326.377049180328
+(1 row)
 
 -- 12. The names of all the players on the Buffalo Bills
+-- Query: nfl=# SELECT players.name, teams.name FROM players FULL OUTER JOIN teams on players.team_id = teams.id WHERE players.team_id = 1;
+-- Output:         name        |     name
+--------------------+---------------
+ Mario Williams     | Buffalo Bills
+ Drayton Florence   | Buffalo Bills
+ Shawne Merriman    | Buffalo Bills
+ Dwan Edwards       | Buffalo Bills
+ Chris Kelsay       | Buffalo Bills
+ Kyle Williams      | Buffalo Bills
+ Nick Barnett       | Buffalo Bills
+ Spencer Johnson    | Buffalo Bills
+ Ryan Fitzpatrick   | Buffalo Bills
+ Steve Johnson      | Buffalo Bills
+ Tyler Thigpen      | Buffalo Bills
+ Lee Evans (Buyout) | Buffalo Bills
+ Brad Smith         | Buffalo Bills
+ Fred Jackson       | Buffalo Bills
+ Scott Chandler     | Buffalo Bills
+ George Wilson      | Buffalo Bills
+ Erik Pears         | Buffalo Bills
+ Leodis McKelvin    | Buffalo Bills
+ Brian Moorman      | Buffalo Bills
+ Terrence McGee     | Buffalo Bills
+ Marcell Dareus     | Buffalo Bills
+ Chad Rinehart      | Buffalo Bills
+ Kraig Urbik        | Buffalo Bills
+ Rian Lindell       | Buffalo Bills
+ Kirk Morrison      | Buffalo Bills
+ Corey McIntyre     | Buffalo Bills
+ C.J. Spiller       | Buffalo Bills
+ Garrison Sanborn   | Buffalo Bills
+ Eric Wood          | Buffalo Bills
+ Lionel Dotson      | Buffalo Bills
+ Ruvell Martin      | Buffalo Bills
+ Andy Levitre       | Buffalo Bills
+ Jairus Byrd        | Buffalo Bills
+ Jarron Gilbert     | Buffalo Bills
+ Kyle Moore         | Buffalo Bills
+ Aaron Williams     | Buffalo Bills
+ Donald Jones       | Buffalo Bills
+ Fendi Onobun       | Buffalo Bills
+ Kellen Heard       | Buffalo Bills
+ Mike Caussin       | Buffalo Bills
+ Naaman Roosevelt   | Buffalo Bills
+ Alex Carrington    | Buffalo Bills
+ Arthur Moats       | Buffalo Bills
+ Colin Brown        | Buffalo Bills
+ Cordaro Howard     | Buffalo Bills
+ Dan Batten         | Buffalo Bills
+ David Nelson       | Buffalo Bills
+ Marcus Easley      | Buffalo Bills
+ Sam Young          | Buffalo Bills
+ Torell Troup       | Buffalo Bills
+ Kelvin Sheppard    | Buffalo Bills
+ Lee Smith          | Buffalo Bills
+ Chris Hairston     | Buffalo Bills
+ Chris White        | Buffalo Bills
+ DaNorris Searcy    | Buffalo Bills
+ Johnny White       | Buffalo Bills
+ Justin Rogers      | Buffalo Bills
+ Kamar Aiken        | Buffalo Bills
+ Michael Jasper     | Buffalo Bills
+(59 rows)
 
 -- 13. The total salary of all players on the New York Giants
-  
+-- Query: nfl=# SELECT SUM(players.salary) FROM players FULL OUTER JOIN teams ON players.team_id = teams.id WHERE teams.name = 'New York Giants';
+-- Output:    sum
+----------
+ 74179466
+(1 row)
 
 -- 14. The player with the lowest salary on the Green Bay Packers
+-- Query: nfl=# SELECT * FROM players WHERE team_id = 23 ORDER BY salary ASC LIMIT 1;
+-- Output:   id  |      name      | position | salary | team_id
+------+----------------+----------+--------+---------
+ 1108 | Shaky Smithson | WR       | 390000 |      23
+(1 row)
  
