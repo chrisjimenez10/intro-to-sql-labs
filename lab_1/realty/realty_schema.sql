f1e3ab7b021a83764a19b61774a4d9f78c40aa30
chris <jimenezchristopher5@gmail.com>
2024-06-04 16:13:16 -0500

    added schema and data seed for realty lab

--- lab_1/realty/realty_schema.sql
+++ lab_1/realty/realty_schema.sql
@@ -1,1 +1,30 @@
--- enter your schema for apartments, offices and storefronts below
+CREATE TABLE apartments(
+    id SERIAL PRIMARY KEY,
+    tenant BOOLEAN NOT NULL,
+    price MONEY NOT NULL,
+    landlord_name VARCHAR(20)
+);
+
+CREATE TABLE offices(
+    id SERIAL PRIMARY KEY,
+    bathrooms INT,
+    cubicles INT,
+    company VARCHAR(20)
+);
+
+CREATE TABLE companies(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) UNIQUE NOT NULL,
+    location VARCHAR(20) NOT NULL,
+    industry VARCHAR(20),
+    employees INT
+);
+
+CREATE TABLE storefronts(
+    id SERIAL PRIMARY KEY,
+    kitchens BOOLEAN,
+    square_footage INT,
+    outdoor_seating BOOLEAN,
+    company_id INT REFERENCES companies(id)
+);
+
--- lab_1/realty/realty_seed.sql
+++ lab_1/realty/realty_seed.sql
@@ -1,1 +1,13 @@
---enter your seed data below
+INSERT INTO companies (name, location, industry, employees) VALUES ('Google', 'San Diego, CA', 'IT', 100000);
+INSERT INTO companies (name, location, industry, employees) VALUES ('Apple', 'New York City, NY', 'IT', 90000);
+INSERT INTO companies (name, location, industry, employees) VALUES ('Tesla', 'Los Angeles, CA', 'IT', 75000);
+INSERT INTO companies (name, location, industry, employees) VALUES ('Amazon', 'Seattle, WA', 'E-Commerce', 150000);
+INSERT INTO companies (name, location, industry, employees) VALUES ('Salesforce', 'Chicago, IL', 'Sales', 45000);
+INSERT INTO companies (name, location, industry, employees) VALUES ('Netflix', 'Tampa Bay, FL', 'Entertainment', 65000);
+
+INSERT INTO apartments (tenant, price, landlord_name) VALUES (TRUE, '$450000', 'Richard Guthrie');
+INSERT INTO apartments (tenant, price, landlord_name) VALUES (FALSE, '$650000', 'Elizabeth Smith');
+INSERT INTO apartments (tenant, price, landlord_name) VALUES (TRUE, '$750000', 'Alejandro Gomez');
+INSERT INTO apartments (tenant, price, landlord_name) VALUES (TRUE, '$350000', 'Tina Camila');
+INSERT INTO apartments (tenant, price, landlord_name) VALUES (FALSE, '$950000', 'Pablo Pueblo');
+INSERT INTO apartments (tenant, price, landlord_name) VALUES (TRUE, '$850000', 'Troy Newton');
